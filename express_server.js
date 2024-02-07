d2073c0f97aef0e3d15c82fd2ef043c42a04e273
Rouslan Lakhno <rouslanlakhno@gmail.com>
2024-02-07 10:40:21 -0500

    Add rendering and urls_index in view folder

--- express_server.js
+++ express_server.js
@@ -18,7 +18,17 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  const templateVars = { greeting: "Hello World!" };
+  res.render("hello_world", templateVars);
+});
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.listen(PORT, () => {
