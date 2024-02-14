a570b4a4466e696ff76bde66a8be4b857b52873b
Rouslan Lakhno <rouslanlakhno@gmail.com>
2024-02-14 12:24:36 -0500

    Modify your login endpoint to use bcrypt to check the password

--- express_server.js
+++ express_server.js
@@ -42,13 +42,22 @@ const users = {
   userRandomID: {
     id: "userRandomID",
     email: "user@example.com",
-    password: "purple-monkey-dinosaur",
+    // password: bcrypt.hashSync("purple-monkey-dinosaur", 10)
+    password: "$2a$10$JmKWQ10g8UxI8w99zmAaiecYqvhCQCFrt1aYY79IBwzV6f9fDlmum"
   },
   user2RandomID: {
     id: "user2RandomID",
     email: "user2@example.com",
-    password: "dishwasher-funk",
+    // password: bcrypt.hashSync("dishwasher-funk", 10)
+    password: "$2a$10$w3Vg0dZ34PybcfHZzax5kuLvf1wmN4I47WVrUJu.q7RulcQCygrSG"
   },
+  // Added user for testing purposes
+  user3RandomID: {
+    id: 'user3RandomID',
+    email: 'user3@example.com',
+    // password: bcrypt.hashSync("1111111", 10)
+    password: '$2a$10$9n/BaR5ERZTOyVdyNNm3uOfj6K13ywgAmdYM0ZYk6JIOrgXgtlYj6'
+  }
 };
 
 // ------------------ Functions -------------------------
@@ -335,18 +344,19 @@ app.post("/urls/:id", (req, res) => {
 });
 
 
-// post Redirect to Edit
+// post Redirect to Edit -------------------
 app.post("/urls/:id/edit", (req, res) => {
   const id = req.params.id;
   res.redirect(`/urls/${id}`); // Respond with 'Ok' (we will replace this)
 });
 
-// POST /login endpoint
+// POST /login endpoint -------------------
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
-  const user = getUserByEmail(email); // Implement this function to find user by email
-
-  if (!user || user.password !== password) {
+  // Implement this function to find user by email
+  const user = getUserByEmail(email); 
+  //   to use bcrypt to check the password
+  if (!user || !bcrypt.compareSync(password, user.password)) {
     res.status(403).send("Invalid email or password");
     return;
   }
